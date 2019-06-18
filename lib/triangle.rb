4be4f78493ae3a2d0e6eeaf6169e6bc08c1cc8e8
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-18 22:06:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,16 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
-<<<<<<< HEAD
-  
-=======
+
   @@all_sides = []
->>>>>>> c8f24d79b27da7f8a1874676459af0848514ca2c
+
   def initialize (side1,side2,side3)
   @side1 = side1
   @side2 = side2
   @side3 = side3 
-<<<<<<< HEAD
   @@all_sides = [side1,side2,side3]
-=======
+
   @@all_sides  << self
->>>>>>> c8f24d79b27da7f8a1874676459af0848514ca2c
-  end
+end
   
   def valid? 
     if(@@all_sides.none? {|side| side <= 0}) && (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
